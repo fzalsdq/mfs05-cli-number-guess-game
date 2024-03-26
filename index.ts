5811db0e23fd8c2572e5fb5e27a1134d4843e17c
M.Faisal Siddiqi <sdq7519@gmail.com>
2024-03-26 12:13:19 +0500

    Removed extra brace in code.

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const answer=await inquirer.prompt([
     message: "Enter a number between 1-10", type:"number", name:"GuessNumber",
     },
 ]);
-}
+
 if (answer.GuessNumber===randomNumber){
     console.log("You guessed right");
 }
